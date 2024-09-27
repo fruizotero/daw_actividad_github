c5d2af3efd5d8acd1050ddac86ce7d43db84888f
fruizotero <frank.git96@gmail.com>
2024-09-27 18:41:39 +0200

    Creación de index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>git-github</title>
+</head>
+<body>
+    <h1>Crea un index.php súbelo al repositorio</h1>
+</body>
+</html>
